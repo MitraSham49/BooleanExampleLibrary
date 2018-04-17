ceb46fd2f43ee15b3c82eb881fb0f3c377483b25
aoa4eva <hi@aoa4eva.me>
2018-04-16 20:19:50 -0400

    Example with boolean values

---
+++ src/main/java/me/afua/booleanexample/BookRepository.java
@@ -0,0 +1,6 @@
+package me.afua.booleanexample;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book,Long>{
+}
---
+++ src/main/java/me/afua/booleanexample/BooleanexampleApplication.java
@@ -0,0 +1,12 @@
+package me.afua.booleanexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooleanexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooleanexampleApplication.class, args);
+    }
+}
